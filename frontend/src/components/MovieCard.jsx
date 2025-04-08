d560bdc0862ea4766001c179adb67d9e38486854
Yash Hokte <hokteyash4@gmail.com>
2025-04-08 21:23:42 +0530

    Implemented Context API and completed mini Project

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,26 +1,37 @@
-import "../css/MovieCard.css"
+import { useMovieContext } from "../context/MovieContext";
+import "../css/MovieCard.css";
 
-const MovieCard = ({movie}) => {
-    
-    const onFavClick = () => {
-        alert('Fav clicked');
-    }
-    return(
-        <div className="movie-card">
-            <div className="movie-poster">
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <div className="movie-overlay">
-                    <button className="favorite-icon" onClick={onFavClick}>
-                        ♥
-                    </button>
-                </div>
-            </div>
-            <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p>{movie.release_date}</p>
-            </div>
+const MovieCard = ({ movie }) => {
+  const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+  const favorite = isFavorite(movie?.id);
+  const onFavClick = (e) => {
+    e.preventDefault();
+    if (favorite) removeFromFavorites(movie?.id);
+    else addToFavorites(movie);
+  };
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""} `}
+            onClick={onFavClick}
+          >
+            ♥
+          </button>
         </div>
-    )
-}
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,10 +1,19 @@
-import "../css/Favorites.css"
+import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../context/MovieContext";
+import "../css/Favorites.css";
 
 const Favorites = () => {
+  const { favorites } = useMovieContext();
   return (
-    <div className="favorites-empty">
-      <h2>Favorite Page</h2>
-      <p>Starting adding your favorite items and they will appear here</p>
+    <div className="movies-grid">
+      {favorites.length == 0 ? (
+        <div className="favorites-empty">
+          <h2>No Movies Found :(</h2>
+          <p>Starting adding your favorite items and they will appear here</p>
+        </div>
+      ) : (
+        favorites.map((movie) => <MovieCard movie={movie} key={movie.id} />)
+      )}
     </div>
   );
 };
