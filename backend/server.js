890a49b5768daafc67bb42b23549470487d896e5
Yash Hokte <hokteyash4@gmail.com>
2025-05-16 12:36:20 +0530

    Added backend project files

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+console.log(process.env.MONGO_URI);
+
+// MongoDB connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.error(JSON.stringify(err)));
